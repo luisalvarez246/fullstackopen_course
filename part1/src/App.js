0e1f7a0e2efef8fa00b218f3c4b1741dd5d7e5ed
lualvare42 <lualvare@Lualvares-MacBook.local>
2023-04-10 23:20:25 +0200

    Ex 1.6 & 1.7 done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,13 +1,4 @@
-const	Header = (props) =>
-{
-	return (
-		<div>
-			<h1>{props.course}</h1>
-		</div>
-	)
-}
-
-const	Part = (props) =>
+/*const	Part = (props) =>
 {
 	console.log(props);
 	return (
@@ -66,6 +57,122 @@ const	App = () =>
 			<Header course = {course.name} />
 			<Content parts= {course.parts} />
 			<Total parts= {course.parts} />
+		</>
+	)
+}
+*/
+
+import { useState } from "react"
+
+const	Header = (props) =>
+{
+	return (
+		<div>
+			<h1>{props.header}</h1>
+		</div>
+	)
+}
+
+const	Display = ({text, rate, text2}) =>
+{
+	return (
+		<div>
+			<p>{text} {rate} {text2}</p> 
+		</div>
+	)
+}
+
+const	Button = ({handleClick, text}) =>
+{
+	return (
+		<button onClick = {handleClick}>
+			{text}
+		</button>
+	)
+}
+
+const App = () =>
+{
+	const	header =
+	{
+		headerOne: "give feedback",
+		headerTwo: "statistics"
+	}
+	const	[good, setGood] = useState(0);
+	const	[neutral, setNeutral] = useState(0);
+	const	[bad, setBad] = useState(0);
+	const	[total, setTotal] = useState(0);
+	const	[accumulated, setAccumulated] = useState(0);
+	const	[average, setAverage] = useState(0);
+	const	[positive, setPositive] = useState(0);
+	
+	const	increaseGood = () => 
+	{
+		const updatedGood = good + 1;
+		const updatedTotal = total + 1;
+		const updatedAccumulated = accumulated + 1;
+		const updatedAverage = updatedAccumulated / updatedTotal;
+		const updatedPositive = (updatedGood / updatedTotal) * 100;
+		
+		setGood(updatedGood);
+		setTotal(updatedTotal);
+		setAccumulated(updatedAccumulated)
+		setAverage(updatedAverage);
+		setPositive(updatedPositive);
+	}
+	const	increaseNeutral = () => 
+	{
+		const updatedTotal = total + 1;
+		const updatedAccumulated = accumulated + 0;
+		const updatedAverage = updatedAccumulated / updatedTotal;
+		const totalPositives = good;
+		const updatedPositive = (totalPositives / updatedTotal) * 100;
+
+
+		setNeutral(neutral + 1);
+		setTotal(updatedTotal);
+		setAccumulated(updatedAccumulated)
+		setAverage(updatedAverage);
+		setPositive(updatedPositive);
+	}
+	const	increaseBad = () => 
+	{
+		const updatedBad = bad + 1;
+		const updatedTotal = total + 1;
+		const updatedAccumulated = accumulated - 1;
+		const updatedAverage = updatedAccumulated / updatedTotal;
+		const totalPositives = good;
+		const updatedPositive = (totalPositives / updatedTotal) * 100;
+
+		setBad(updatedBad);
+		setTotal(updatedTotal);
+		setAccumulated(updatedAccumulated)
+		setAverage(updatedAverage);
+		setPositive(updatedPositive);
+	}
+	console.log(total, average);
+	return (
+		<>
+			<Header header = {header.headerOne} />
+			<Button  
+				handleClick = {increaseGood}
+				text = "good"
+			/>
+			<Button  
+				handleClick = {increaseNeutral}
+				text = "neutral"
+			/>
+			<Button  
+				handleClick = {increaseBad}
+				text = "bad"
+			/>
+			<Header header = {header.headerTwo} />
+			<Display text = "good" rate = {good} />
+			<Display text = "neutral" rate = {neutral}/> 
+			<Display text = "bad" rate = {bad} />
+			<Display text = "all" rate = {total} />
+			<Display text = "average" rate = {average} />
+			<Display text = "positive" rate = {positive} text2 = " %"/>
 		</>
 	)
 }
