e76036b2309cd84e8bbaffb6601640514c37f894
lualvare42 <lualvare@Lualvares-MacBook.local>
2023-04-11 10:59:22 +0200

    Ex 1.8 done

--- part1/src/App.js
+++ part1/src/App.js
@@ -73,11 +73,25 @@ const	Header = (props) =>
 	)
 }
 
-const	Display = ({text, rate, text2}) =>
+const	Statistics = ({text, rate, text2}) =>
 {
 	return (
 		<div>
 			<p>{text} {rate} {text2}</p> 
+		</div>
+	)
+}
+
+const	Display = ({rate}) =>
+{
+	return (
+		<div>
+			<Statistics text = {rate[0].text} rate = {rate[0].value} />
+			<Statistics text = {rate[1].text} rate = {rate[1].value} />
+			<Statistics text = {rate[2].text} rate = {rate[2].value} />
+			<Statistics text = {rate[3].text} rate = {rate[3].value} />
+			<Statistics text = {rate[4].text} rate = {rate[4].value} />
+			<Statistics text = {rate[5].text} rate = {rate[5].value} text2 = {rate[5].text2} />
 		</div>
 	)
 }
@@ -105,14 +119,42 @@ const App = () =>
 	const	[accumulated, setAccumulated] = useState(0);
 	const	[average, setAverage] = useState(0);
 	const	[positive, setPositive] = useState(0);
+	const	rate =
+	[		
+		{
+			text: "good",
+			value: good,
+		},	
+		{
+			text: "neutral",
+			value: neutral,
+		},
+		{
+			text: "bad",
+			value: bad,
+		},	
+		{
+			text: "total",
+			value: total,
+		},
+		{
+			text: "average",
+			value: average,
+		},
+		{
+			text: "positive",
+			value: positive,
+			text2: " %"
+		},
+	]
 	
 	const	increaseGood = () => 
 	{
-		const updatedGood = good + 1;
-		const updatedTotal = total + 1;
-		const updatedAccumulated = accumulated + 1;
-		const updatedAverage = updatedAccumulated / updatedTotal;
-		const updatedPositive = (updatedGood / updatedTotal) * 100;
+		const 	updatedGood = good + 1;
+		const 	updatedTotal = total + 1;
+		const 	updatedAccumulated = accumulated + 1;
+		const 	updatedAverage = updatedAccumulated / updatedTotal;
+		const 	updatedPositive = (updatedGood / updatedTotal) * 100;
 		
 		setGood(updatedGood);
 		setTotal(updatedTotal);
@@ -122,11 +164,11 @@ const App = () =>
 	}
 	const	increaseNeutral = () => 
 	{
-		const updatedTotal = total + 1;
-		const updatedAccumulated = accumulated + 0;
-		const updatedAverage = updatedAccumulated / updatedTotal;
-		const totalPositives = good;
-		const updatedPositive = (totalPositives / updatedTotal) * 100;
+		const 	updatedTotal = total + 1;
+		const 	updatedAccumulated = accumulated + 0;
+		const 	updatedAverage = updatedAccumulated / updatedTotal;
+		const 	totalPositives = good;
+		const 	updatedPositive = (totalPositives / updatedTotal) * 100;
 
 
 		setNeutral(neutral + 1);
@@ -137,12 +179,12 @@ const App = () =>
 	}
 	const	increaseBad = () => 
 	{
-		const updatedBad = bad + 1;
-		const updatedTotal = total + 1;
-		const updatedAccumulated = accumulated - 1;
-		const updatedAverage = updatedAccumulated / updatedTotal;
-		const totalPositives = good;
-		const updatedPositive = (totalPositives / updatedTotal) * 100;
+		const 	updatedBad = bad + 1;
+		const 	updatedTotal = total + 1;
+		const 	updatedAccumulated = accumulated - 1;
+		const 	updatedAverage = updatedAccumulated / updatedTotal;
+		const 	totalPositives = good;
+		const 	updatedPositive = (totalPositives / updatedTotal) * 100;
 
 		setBad(updatedBad);
 		setTotal(updatedTotal);
@@ -151,6 +193,7 @@ const App = () =>
 		setPositive(updatedPositive);
 	}
 	console.log(total, average);
+	console.log(rate[0].value);
 	return (
 		<>
 			<Header header = {header.headerOne} />
@@ -166,13 +209,8 @@ const App = () =>
 				handleClick = {increaseBad}
 				text = "bad"
 			/>
-			<Header header = {header.headerTwo} />
-			<Display text = "good" rate = {good} />
-			<Display text = "neutral" rate = {neutral}/> 
-			<Display text = "bad" rate = {bad} />
-			<Display text = "all" rate = {total} />
-			<Display text = "average" rate = {average} />
-			<Display text = "positive" rate = {positive} text2 = " %"/>
+			<Header	header = {header.headerTwo} />
+			<Display rate = {rate} />
 		</>
 	)
 }
