a8ad624ca5fc8e83200ae4db2942e0b2c548e6ed
lualvare42 <lualvare@Lualvares-MacBook.local>
2023-04-12 07:38:47 +0200

    Ex 1.13 1.14 done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -9,11 +9,30 @@ const	Button = ({handleClick, text}) =>
 	)
 }
 
-const 	Display = ({text}) =>
+const 	Display = ({text, text2, votes, text3}) =>
 {
 	return (
 		<div>
-			<p>{text}</p>
+			<h4>{text}</h4>
+			<p>{text2} {votes} {text3}</p>
+		</div>
+	)
+}
+
+const	DisplayFavorite = ({numberOfVotes, text, text2, votes, text3}) =>
+{
+	if (numberOfVotes === 0)
+	{
+		return (
+			<div>
+				<p>No votes yet</p>
+			</div>
+		)
+	}
+	return (
+		<div>
+			<h4>{text}</h4>
+			<p>{text2} {votes} {text3}</p>
 		</div>
 	)
 }
@@ -30,8 +49,9 @@ const	App = () =>
 		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 		'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
 		'The only way to go fast, is to go well.'
-	]   
+	];
 	const	[selected, setSelected] = useState(0);
+	const	[voted, setVoted] = useState(Array(8).fill(0));
 
 	const	randomSelected = () =>
 	{
@@ -39,14 +59,31 @@ const	App = () =>
 
 		setSelected(randomNumber);
 	}
+	const	increaseVoted = () =>
+	{
+		const	updateVoted = [...voted];
 
+		updateVoted[selected] += 1;
+		setVoted(updateVoted);
+	}
+	const	numberOfVotes = Math.max(...voted);
+	const	topVoted = voted.indexOf(numberOfVotes);
+	console.log(voted);
+	console.log(topVoted);
 	return (
 		<>
-			<Display text = {anecdotes[selected]} />
+			<Display text = {anecdotes[selected]}
+				text2 = "has" votes = {voted[selected]} text3 = "votes"/>
+			<Button 
+				handleClick = {increaseVoted}
+				text = "vote"
+			/>
 			<Button 
 				handleClick = {randomSelected}
 				text = "next anecdote"
 			/>
+			<DisplayFavorite text = {anecdotes[topVoted]} numberOfVotes = {numberOfVotes} 
+				text2 = "has" votes = {voted[topVoted]} text3 = "votes"/>
 		</>
 	)
 }
