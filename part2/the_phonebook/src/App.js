8b6de01a59671e42335e581c6b04c37185d0db4c
lualvare42 <lualvare@Lualvares-MacBook.local>
2023-04-14 18:00:33 +0200

    Ex 2.8 done
    Implemented a second input element (phone number).
    Added conditional for preventing duplicate phone numbers.
    Added conditional for preventing blank fields from being
    added.

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,27 +1,21 @@
 import { useState } from 'react'
 
-const	Name = ({name}) =>
+const	Contact = ({name, number}) =>
 {
 	return (
-		<p>{name}</p>
+		<p>{name} {number}</p>
 	)
 }
 
 const Add = ({persons, duplicate}) =>
 {
 	console.log(duplicate);
-	/*if (duplicate === 1)
-	{
-		return (
-			<p>There are duplicates :/</p>
-		)
-	}*/
 	return (
 			<div>
 				{
 					persons.map(person =>
 					[
-						<Name name = {person.name} key ={person.id}/>
+						<Contact name = {person.name} number = {person.number} key ={person.id}/>
 					]
 					)
 				}
@@ -31,73 +25,89 @@ const Add = ({persons, duplicate}) =>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-
+    { name: 'Arto Hellas', number: '040-1234567' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const	[newNumber, setNewNumber] = useState('');
+  
   const	addName = (event) =>
   {
 	event.preventDefault();
-	const personObject = 
+	const personObject =
 	{
 		name: newName,
+		number: newNumber,
 		id: (persons.length - 1) + 1
 	}
-	if (duplicateSearch(personObject.name) === 1)
+	if (personObject.name === '' || personObject.number === '')
+	{
+		alert(`No blanks allowed for name or number fields`);
+	}
+	else if (duplicateSearch(personObject.name, null) === 1)
 	{
 		alert(`${personObject.name} is already added to phonebook`);
+	}
+	else if (duplicateSearch(null, personObject.number) === 2)
+	{
+		alert(`The number ${personObject.number} is already added to phonebook`);
 	}
 	else
 	{	
 		setPersons(persons.concat(personObject));
 		setNewName('');
+		setNewNumber('');
 	}
   }
-  const	inputHandle = (event) =>
+  const	nameHandle = (event) =>
   {
 	console.log(event.target.value);
 	setNewName(event.target.value);
   }
-  const	duplicateSearch = (find) =>
+  const	numberHandle = (event) =>
+  {
+	console.log(event.target.value);
+	setNewNumber(event.target.value);
+  }
+  const	duplicateSearch = (name, number) =>
   {
 	for (let i = 0; i < persons.length; i++)
 	{
-		if (persons[i].name === find)
+		if (persons[i].name === name)
 				return (1);
+	}
+	for (let i = 0; i < persons.length; i++)
+	{
+		if (persons[i].number === number)
+				return (2);
 	}
 	return (0);
   }
-  /*const	duplicateSearch = () =>
-  {
-	if (persons.length === 1)
-		return (0);
-	for (let i = 0; i < persons.length; i++)
-	{
-		for (let n = (i + 1); n < persons.length; n++)
-		{
-			if (persons[i].name === persons[n].name)
-				return (1);
-		}
-	}
-	return (0);
-  }*/
   let	duplicate;
+  //let	test_array;
   
   persons[0].id = 0;
   duplicate = duplicateSearch();
-  //console.log(persons[0].name);
+  //test_array = persons.map(string => string.name);
+  //console.log(test_array[1]);
   //console.log(isEqual(persons[0].name, persons[1].name))
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit = {addName}>
-      	<div>debug: {newName}</div>
+      	<div>debug: {newNumber}</div>
         <div>
           name: 
 		  	<input 
 		  		value = {newName}
-				onChange = {inputHandle}
+				onChange = {nameHandle}
+			/>
+        </div>
+        <div>
+          number: 
+		  	<input 
+		  		value = {newNumber}
+				onChange = {numberHandle}
 			/>
         </div>
         <div>
