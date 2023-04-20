b67f1f61d37a0113a326e0da86d76491004c82ce
lualvare42 <lualvare@Lualvares-MacBook.local>
2023-04-20 21:18:54 +0200

    Ex 2.18
    first part of Data For Countries ex.

---
+++ part2/data_for_countries/src/App.js
@@ -0,0 +1,143 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const	Search = ({newSearch, newSearchHandle}) =>
+{
+	return (
+		<div>
+			find countries
+			<input 
+				value = {newSearch}
+				onChange = {newSearchHandle} 
+			/>
+		</div>
+	)
+}
+
+const	Countries = ({name}) =>
+{	
+	console.log(name);
+	return (
+		<p>{name} <button>show</button></p>
+	)
+}
+
+const	Languages = ({language}) =>
+{
+	return (
+	<li>{language}</li>
+	)
+}
+
+const	CountryDetails = ({name, capital, area, languages, flag}) =>
+{
+	const	lang = Object.entries(languages).map(language => language[1]);
+	console.log(flag);
+	return (
+		<div>
+			<h1>{name}</h1>
+			<p>{capital}</p>
+			<p>{area} km2</p>
+			<h3>languages:</h3>
+			{
+				lang.map((l, i) =>
+				[
+					<Languages language = {l} key = {i}/>
+				]
+				)
+			}
+			<br></br>
+			<img src = {flag} alt = 'country flag'/>
+		</div>
+	)
+}
+
+const	Results = ({results}) =>
+{
+	console.log(results);
+	if (!results)
+		return (null);
+	else if (results.length > 10)
+	{
+		return (
+			<div>
+				<p>Too many matches, specify another filter</p>
+			</div>
+		)
+	}
+	else if (results.length === 1)
+	{
+		console.log(Object.entries(results[0].languages).map(language => language[1]));
+		console.log(results[0].flags.png);
+		return (
+			<div>
+				<CountryDetails 
+					name = {results[0].name.common}
+					capital = {results[0].capital[0]}
+					area = {results[0].area}
+					languages = {results[0].languages}
+					flag = {results[0].flags.png}
+				/>
+			</div>
+		)
+	}
+	const	filtered = results.map(country => country.name.common);
+	return (
+		<div>
+		{
+			filtered.map((result, i) =>
+			[
+				<Countries
+					name = {result}
+					key = {i}
+				/>
+			]	
+			)
+		}
+		</div>
+	)
+}
+
+function App() 
+{
+	const	[newSearch, setSearch] = useState('');
+	const	[countries, setCountries] = useState('');
+
+	const	newSearchHandle = (event) =>
+	{
+		console.log(event.target.value);
+		setSearch(event.target.value);
+	}
+	
+	const	hook = () =>
+	{
+		if (newSearch)
+		{
+			console.log('fetching countries...');
+			axios
+				.get(`https://restcountries.com/v3.1/all`)
+				.then(response =>
+				{
+					const	allResults = response.data.filter(country => country.name.common
+						.toLowerCase()
+						.includes(newSearch) === true)
+					//	.map(country => country.name.common);
+					
+					console.log(allResults);
+					setCountries(allResults);
+				}
+				)
+		}
+	}
+	
+	useEffect(hook, [newSearch]);
+
+	return (
+		<>
+			<Search newSearch = {newSearch} newSearchHandle = {newSearchHandle} />
+			<Results results = {countries} />
+		</>
+	);
+}
+
+export default App;
